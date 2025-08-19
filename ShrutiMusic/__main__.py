049eb3439cfb3a83dd4fc122bf9b6803b4bebc4a
AkashOP <akp954834@gmail.com>
2025-08-19 17:40:07 +0530

    Update __main__.py

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 import asyncio
 import importlib
 from pyrogram import idle
@@ -113,6 +91,7 @@ COMMANDS = [
     BotCommand("dare", "💪 Get random dare challenge"),
     BotCommand("mongochk", "🗃 Check MongoDB URL validity"),
     BotCommand("font", "🎨 Convert text to beautiful fonts"),
+    BotCommand("couple", "👏🏻Find Your Couple"),
     BotCommand("gali", "😤 Send random gali"),
     BotCommand("bots", "🤖 Get list of bots in group")
 ]
