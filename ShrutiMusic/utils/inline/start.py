004a300f259b0bf195d6be6912b9b444adb055a5
AkashOP <akp954834@gmail.com>
2025-08-20 10:11:31 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -8,7 +8,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="ping_status"),
         ],
         [
             InlineKeyboardButton(text=_["E_X_1"], callback_data="show_video_panel"),
