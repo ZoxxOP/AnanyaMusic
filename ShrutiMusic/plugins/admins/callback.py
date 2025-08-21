bb805189cf726f480d77e25e94d1e0839f14de6d
AkashOP <akp954834@gmail.com>
2025-08-21 12:32:10 +0530

    Update callback.py

--- ShrutiMusic/plugins/admins/callback.py
+++ ShrutiMusic/plugins/admins/callback.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 import asyncio
 
 from pyrogram import filters
@@ -61,7 +39,7 @@ import config
 checker = {}
 upvoters = {}
 
-Shruti_Repo = "https://files.catbox.moe/tub372.mp4"
+Shruti_Repo = "https://files.catbox.moe/m6yqc3.mp4"
 
 @app.on_callback_query(filters.regex("show_video_panel"))
 async def show_video_callback(_, query):
@@ -673,13 +651,3 @@ async def markup_timer():
 asyncio.create_task(markup_timer())
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
