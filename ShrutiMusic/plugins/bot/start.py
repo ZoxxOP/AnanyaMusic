10b5d687fce30c2017b1bb194f3bde89dc7c5f98
AkashOP <akp954834@gmail.com>
2025-08-29 11:16:56 +0530

    Update start.py

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -1,9 +1,29 @@
-import time 
+import time
 import random
 
-from pyrogram import filters from pyrogram.enums import ChatType from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message from youtubesearchpython.future import VideosSearch
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtubesearchpython.__future__ import VideosSearch
 
-import config from ShrutiMusic import app from ShrutiMusic.misc import boot from ShrutiMusic.plugins.sudo.sudoers import sudoers_list from ShrutiMusic.utils.database import ( add_served_chat, add_served_user, blacklisted_chats, get_lang, is_banned_user, is_on_off, ) from ShrutiMusic.utils import bot_sys_stats from ShrutiMusic.utils.decorators.language import LanguageStart from ShrutiMusic.utils.formatters import get_readable_time from ShrutiMusic.utils.inline import help_pannel_page1, private_panel, start_panel from config import BANNED_USERS from strings import get_string
+import config
+from ShrutiMusic import app
+from ShrutiMusic.misc import boot
+from ShrutiMusic.plugins.sudo.sudoers import sudoers_list
+from ShrutiMusic.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
+from ShrutiMusic.utils import bot_sys_stats
+from ShrutiMusic.utils.decorators.language import LanguageStart
+from ShrutiMusic.utils.formatters import get_readable_time
+from ShrutiMusic.utils.inline import help_pannel_page1, private_panel, start_panel
+from config import BANNED_USERS
+from strings import get_string
 
 Random stickers list
 
