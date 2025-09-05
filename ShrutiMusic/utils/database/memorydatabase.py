338eb516c1ffcddbd399688f4f28088d22f126f3
AkashOP <akp954834@gmail.com>
2025-09-05 21:28:32 +0530

    Update memorydatabase.py

--- ShrutiMusic/utils/database/memorydatabase.py
+++ ShrutiMusic/utils/database/memorydatabase.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 import json
 import os
 from typing import Dict, List, Union
@@ -671,13 +649,3 @@ async def get_video_bitrate(chat_id: int) -> str:
     }.get(mode, VideoQuality.SD_480p)
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
