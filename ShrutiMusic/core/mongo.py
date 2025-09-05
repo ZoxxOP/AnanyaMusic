97e32445ccaa212183f73e29a76e1027508e195d
AkashOP <akp954834@gmail.com>
2025-09-05 21:38:40 +0530

    Update mongo.py

--- ShrutiMusic/core/mongo.py
+++ ShrutiMusic/core/mongo.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
@@ -35,14 +13,3 @@ except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
     exit()
 
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
