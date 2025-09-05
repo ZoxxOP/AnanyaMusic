aa473896bc60b3d51581fbdad5c79c87022ee704
AkashOP <akp954834@gmail.com>
2025-09-05 16:20:43 +0530

    Update wish.py

--- ShrutiMusic/plugins/tools/wish.py
+++ ShrutiMusic/plugins/tools/wish.py
@@ -216,14 +216,3 @@ async def taghelp(_, message: Message):
 """
     await message.reply(help_text)
 
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
