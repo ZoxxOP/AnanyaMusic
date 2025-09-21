840193e0ff2b53aca48b9c40a3330d7225394f58
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-09-21 21:14:11 +0530

    Update start.py

--- ShrutiMusic/plugins/bot/start.py
+++ ShrutiMusic/plugins/bot/start.py
@@ -100,8 +100,8 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         UP, CPU, RAM, DISK = await bot_sys_stats()
-        await message.reply_photo(
-            photo=config.START_IMG_URL,
+        await message.reply_video(
+            photo=config.START_VIDEO_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM),
             reply_markup=InlineKeyboardMarkup(out),
         )
