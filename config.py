0f5f5c322107a9e193dcf32c321d15d1d9990c5b
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-09-21 21:40:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -91,6 +91,7 @@ VIDEO_API_URL = os.getenv("VIDEO_API_URL", "https://api.video.thequickearn.xyz")
 API_KEY = os.getenv("30DxNexGenBots4d6508", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 START_VIDEO_URL = os.getenv("START_VIDEO_URL", "https://files.catbox.moe/1tnq00.mp4")
+START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/hee6pf.jpg")
 PING_IMG_URL = os.getenv("PING_IMG_URL", "https://files.catbox.moe/hee6pf.jpg")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/hee6pf.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/hee6pf.jpg"
