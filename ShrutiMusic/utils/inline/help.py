670c8450705ec1ab074951e9b9fb073a984dabb9
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-09-23 15:35:58 +0530

    Update help.py

--- ShrutiMusic/utils/inline/help.py
+++ ShrutiMusic/utils/inline/help.py
@@ -137,7 +137,7 @@ def help_pannel_page4(_, START: Union[bool, int] = None):
                     text=_["BACK_BUTTON"] if START else _["CLOSE_BUTTON"],
                     callback_data="settingsback_helper" if START else "close",
                 ),
-                InlineKeyboardButton(text="⌯ Nᴇxᴛ ⌯", callback_data="help_page_3"),
+                InlineKeyboardButton(text="⌯ Nᴇxᴛ ⌯", callback_data="help_page_1")
             ],
         ]
     )
