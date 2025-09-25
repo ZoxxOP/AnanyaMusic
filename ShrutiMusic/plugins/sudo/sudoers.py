2d80b5f9e7067df070ab9f1c2f45c0c016a00ef2
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-09-25 07:41:33 +0530

    Update sudoers.py

--- ShrutiMusic/plugins/sudo/sudoers.py
+++ ShrutiMusic/plugins/sudo/sudoers.py
@@ -55,7 +55,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("❍ Uff.. 🤪 ●", show_alert=True)
+        return await callback_query.answer("❍ Uff.. 🤪 ● ᴘᴀʜʟᴇ ᴊᴀᴋᴇ ᴋʜᴜᴅ ʙᴏᴛ ʙɴᴀ ʟ#ᴜᴅᴇ", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
